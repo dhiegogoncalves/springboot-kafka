a5021ce410c23dcd72949866ade2c3311ecfa8d8
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-10-09 16:47:27 -0400

    add callback function to sendMessage function.

--- producer/src/main/java/com/springboot/producer/services/ProducerService.java
+++ producer/src/main/java/com/springboot/producer/services/ProducerService.java
@@ -4,7 +4,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class ProducerService {
@@ -12,6 +14,8 @@ public class ProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("topic", message);
+        kafkaTemplate.send("topic", message).addCallback(
+                sucess -> log.info("Send message with success {}", message),
+                error -> log.error("Error send message"));
     }
 }
